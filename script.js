c9a946ec441f44e1d380a226bd5d082f12ca1a30
felipe-tneves <felipe.tadeusantos1@gmail.com>
2024-05-26 11:00:20 -0300

    feat: Aplicando lógica para o Mario pular

---
+++ script.js
@@ -0,0 +1,11 @@
+let mario = document.querySelector('.mario')
+
+const jump = () =>{
+    mario.classList.add('jump');
+
+    setTimeout(()=>{
+        mario.classList.remove('jump')
+    },500);
+}
+
+document.addEventListener('keydown', jump);
